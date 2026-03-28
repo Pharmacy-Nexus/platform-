f12437b051cc71a8e432d70f3a0a5d8f13a9fdab
Ahmed Nabil <nobel4530@gmail.com>
2026-03-28 23:36:20 +0200

    Update core.js

--- intern/js/core.js
+++ intern/js/core.js
@@ -9,8 +9,9 @@
   config: 'pn_intern_config_v1',
   topicsCache: 'pn_intern_topics_cache_v1',
   session: 'pn_intern_session_v1',
-  practiceReview: 'pn_intern_practice_review_v1'
-      },
+  practiceReview: 'pn_intern_practice_review_v1',
+  practiceRetry: 'pn_intern_practice_retry_v1'
+},
       supabase: {
         url: '',
         anonKey: ''
